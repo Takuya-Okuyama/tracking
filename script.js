3b9f0b2e54b9783c8d5ee2ec49fcb245f7047416
Takuya-Okuyama <95153595+Takuya-Okuyama@users.noreply.github.com>
2023-12-13 14:49:54 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 let watchId = null; // watchPositionから得られるIDを格納する変数
 let lastSavedTime = Date.now(); // 最後に保存した時刻
 let isTracking = false;
-let fileHandle;
 const sampling_interval = 10 * 1000; // ミリ秒
+
+// データベースの初期設定
+let db;
+let savefilepath;
+const request = indexedDB.open("LocationsDB", 1);
+
+request.onerror = function (event) {
+    console.error("Database error: " + event.target.errorCode);
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+};
+
+request.onupgradeneeded = function (event) {
+    let db = event.target.result;
+    db.createObjectStore("locations", { autoIncrement: true });
+};
 
 // 位置情報を取得してローカルストレージに保存する関数
 function saveLocation(position) {
@@ -26,17 +43,19 @@ function saveLocation(position) {
         document.getElementById('timestamp').innerText = now.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' });
         document.getElementById('latitude').innerText = locationData.lat;
         document.getElementById('longitude').innerText = locationData.lon;
-        document.getElementById('altitude').innerText = locationData.alt !== null ? locationData.alt : "N/A";
-        document.getElementById('accuracy').innerText = locationData.acc !== null ? locationData.acc : "N/A";
-        document.getElementById('altitudeAccuracy').innerText = locationData.altacc !== null ? locationData.altacc : "N/A";
-        document.getElementById('heading').innerText = locationData.head !== null ? locationData.head : "N/A";
-        document.getElementById('speed').innerText = locationData.spd !== null ? locationData.spd : "N/A";
+        document.getElementById('altitude').innerText = locationData.alt !== null ? locationData.alt : 720;
+        document.getElementById('accuracy').innerText = locationData.acc !== null ? locationData.acc : 720;
+        document.getElementById('altitudeAccuracy').innerText = locationData.altacc !== null ? locationData.altacc : -1;
+        document.getElementById('heading').innerText = locationData.head !== null ? locationData.head : -1;
+        document.getElementById('speed').innerText = locationData.spd !== null ? locationData.spd : -1;
 
         // ローカルストレージに保存する前に、以前のデータを配列として取得
-        let locations = JSON.parse(localStorage.getItem('locations')) || [];
-        locations.push(locationData);
-        localStorage.setItem('locations', JSON.stringify(locations));
-        lastSavedTime = currentTime;
+        //let locations = JSON.parse(localStorage.getItem('locations')) || [];
+        //locations.push(locationData);
+        ////localStorage.setItem('locations', JSON.stringify(locations));
+        let transaction = db.transaction(["locations"], "readwrite");
+        let store = transaction.objectStore("locations");
+        store.add(locationData);
     }
 }
 
@@ -54,15 +73,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // ファイル名
         const now = new Date();
-        const filepath = "LocationTracker_" + now.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' });
-        alert(filepath);
-
-        const textContent = "書き込みたい内容";
-        fileHandle = await window.showSaveFilePicker({
-            suggestedName: filepath
-        });
-        await writeFile(fileHandle, textContent);
-        console.log('書き込み完了');
+        savefilepath = "LocationTracker_" + now.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
         if (navigator.geolocation) {
             // watchPositionメソッドを使用して位置情報の追跡を開始
@@ -74,8 +85,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // トラッキング開始時刻を表示
             const now = new Date();
-            const formattedTime = now.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' });
-            document.getElementById('trackingStartTime').innerText = `Tracking started at: ${formattedTime}`;
+            const strTime = now.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' });
+            document.getElementById('trackingStartTime').innerText = `Tracking started at: ${strTime}`;
 
             // ボタンの表示を変更
             document.getElementById('trackingButton').innerText = 'Stop Tracking and Save Data';
@@ -89,44 +100,40 @@ document.addEventListener('DOMContentLoaded', function () {
         isTracking = true;
     }
 
-    function downloadData() {
+    async function downloadData() {
         // データのダウンロードを行うコードをここに記述
         console.log("Downloading data...");
 
-        // Retrieve the location data from local storage
-        const locations = localStorage.getItem('locations');
-        if (locations) {
-            // Create a Blob from the data
-            const blob = new Blob([locations], { type: 'text/plain' });
-            const url = URL.createObjectURL(blob);
+        let transaction = db.transaction(["locations"], "readonly");
+        let store = transaction.objectStore("locations");
+        let request = store.getAll();
 
-            // Create a link and trigger the download
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = 'locations.txt';
-            document.body.appendChild(a);
-            a.click();
+        request.onerror = function (event) {
+            console.error("Error fetching data: ", event.target.errorCode);
+        };
 
-            // Clean up by revoking the Object URL and removing the link
-            URL.revokeObjectURL(url);
-            document.body.removeChild(a);
+        request.onsuccess = function (event) {
+            let data = event.target.result;
+            download(JSON.stringify(data, null, 2), savefilepath, "text/plain");
+        };
 
-            // トラッキング修了時刻を表示
-            const now = new Date();
-            const formattedTime = now.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' });
-            document.getElementById('trackingEndTime').innerText = `Tracking ended at: ${formattedTime}`;
-
-            document.getElementById('trackingButton').disabled = true;
-            // ボタンの表示を元に戻す
-            //document.getElementById('trackingButton').innerText = 'Done';
-            //isTracking = false;
-        } else {
-            console.log('No location data to download.');
-        }
+        // トラッキング修了時刻を表示
+        const now = new Date();
+        const formattedTime = now.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' });
+        document.getElementById('trackingEndTime').innerText = `Tracking ended at: ${formattedTime}`;
 
         // ボタンの表示を元に戻す
         document.getElementById('trackingButton').innerText = 'Start Tracking';
         isTracking = false;
+    }
+
+    // データをダウンロードするための補助関数
+    function download(content, fileName, contentType) {
+        let a = document.createElement("a");
+        let file = new Blob([content], { type: contentType });
+        a.href = URL.createObjectURL(file);
+        a.download = fileName;
+        a.click();
     }
 
     // イベントリスナーを追加
@@ -138,14 +145,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-
-async function writeFile(fileHandle, contents) {
-    // writable作成
-    const writable = await fileHandle.createWritable();
-
-    // コンテンツを書き込む
-    await writable.write(contents);
-
-    // ファイル閉じる
-    await writable.close();
-}
