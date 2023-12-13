6aa44fdf143b3f3a9f282ee892555088dfc2f000
Takuya-Okuyama <95153595+Takuya-Okuyama@users.noreply.github.com>
2023-12-13 03:56:06 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 let watchId = null; // watchPositionから得られるIDを格納する変数
 let lastSavedTime = Date.now(); // 最後に保存した時刻
+let isTracking = false;
+const sampling_interval = 10 * 1000; // ミリ秒
 
 // 位置情報を取得してローカルストレージに保存する関数
 function saveLocation(position) {
     const currentTime = Date.now();
 
-    // 前回の保存から2秒以上経過しているか確認
-    if (currentTime - lastSavedTime >= 2000) {
+    // 前回の保存から10秒以上経過しているか確認
+    if (currentTime - lastSavedTime >= sampling_interval) {
         const locationData = {
-            timestamp: new Date(position.timestamp).toISOString(),
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            altitude: position.coords.altitude || null,
-            accuracy: position.coords.accuracy || null,
-            //altitudeAccuracy: position.coords.altitudeAccuracy,
-            heading: position.coords.heading || null,
-            speed: position.coords.speed || null
+            time: new Date(position.timestamp).toISOString(),
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+            alt: position.coords.altitude || null,
+            acc: position.coords.accuracy || null,
+            altacc: position.coords.altitudeAccuracy || null,
+            head: position.coords.heading || null,
+            spd: position.coords.speed || null
         };
 
         // テーブルに位置情報を追加
-        const row = document.getElementById("locationTable").insertRow(-1);
-        row.insertCell(0).innerHTML = locationData.timestamp;
-        row.insertCell(1).innerHTML = locationData.latitude;
-        row.insertCell(2).innerHTML = locationData.longitude;
-        row.insertCell(3).innerHTML = locationData.altitude !== null ? locationData.altitude : "N/A";
-        row.insertCell(4).innerHTML = locationData.accuracy !== null ? locationData.accuracy : "N/A";
-        row.insertCell(5).innerHTML = locationData.heading !== null ? locationData.heading : "N/A";
-        row.insertCell(6).innerHTML = locationData.speed !== null ? locationData.speed : "N/A";
+        const now = new Date();
+        document.getElementById('timestamp').innerText = now.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' });
+        document.getElementById('latitude').innerText = locationData.lat;
+        document.getElementById('longitude').innerText = locationData.lon;
+        document.getElementById('altitude').innerText = locationData.alt !== null ? locationData.alt : "N/A";
+        document.getElementById('accuracy').innerText = locationData.acc !== null ? locationData.acc : "N/A";
+        document.getElementById('altitudeAccuracy').innerText = locationData.altacc !== null ? locationData.altacc : "N/A";
+        document.getElementById('heading').innerText = locationData.head !== null ? locationData.head : "N/A";
+        document.getElementById('speed').innerText = locationData.spd !== null ? locationData.spd : "N/A";
 
         // ローカルストレージに保存する前に、以前のデータを配列として取得
         let locations = JSON.parse(localStorage.getItem('locations')) || [];
@@ -36,52 +39,89 @@ function saveLocation(position) {
     }
 }
 
-// 位置情報の追跡を開始
-function startTracking() {
-    if (navigator.geolocation) {
-        // watchPositionメソッドを使用して位置情報の追跡を開始
-        watchId = navigator.geolocation.watchPosition(saveLocation, handleError, {
-            enableHighAccuracy: true,
-            timeout: 100000,
-            maximumAge: 0
-        });
-    } else {
-        alert("Geolocation is not supported by this browser.");
-    }
-}
-
 // 位置情報取得のエラーハンドリング
 function handleError(error) {
     alert(`ERROR(${error.code}): ${error.message}`);
 }
 
-// 位置情報の追跡を停止
-function stopTracking() {
-    if (watchId !== null) {
-        navigator.geolocation.clearWatch(watchId);
-        watchId = null;
+document.addEventListener('DOMContentLoaded', function () {
+    let isTracking = false;
+
+    function startTracking() {
+        // 位置情報の追跡を開始するコードをここに記述
+        console.log("Tracking started...");
+
+        if (navigator.geolocation) {
+            // watchPositionメソッドを使用して位置情報の追跡を開始
+            watchId = navigator.geolocation.watchPosition(saveLocation, handleError, {
+                enableHighAccuracy: true,
+                timeout: 100 * 1000,
+                maximumAge: 0
+            });
+
+            // トラッキング開始時刻を表示
+            const now = new Date();
+            const formattedTime = now.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' });
+            document.getElementById('trackingStartTime').innerText = `Tracking started at: ${formattedTime}`;
+            
+            // ボタンの表示を変更
+            document.getElementById('trackingButton').innerText = 'Stop Tracking and Save Data';
+            isTracking = true;
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+
+        // ボタンの表示を変更
+        document.getElementById('trackingButton').innerText = 'Stop Tracking and Save Data';
+        isTracking = true;
     }
-}
 
-function downloadData() {
-    // Retrieve the location data from local storage
-    const locations = localStorage.getItem('locations');
-    if (locations) {
-        // Create a Blob from the data
-        const blob = new Blob([locations], { type: 'text/plain' });
-        const url = URL.createObjectURL(blob);
+    function downloadData() {
+        // データのダウンロードを行うコードをここに記述
+        console.log("Downloading data...");
 
-        // Create a link and trigger the download
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'locations.txt';
-        document.body.appendChild(a);
-        a.click();
+        // Retrieve the location data from local storage
+        const locations = localStorage.getItem('locations');
+        if (locations) {
+            // Create a Blob from the data
+            const blob = new Blob([locations], { type: 'text/plain' });
+            const url = URL.createObjectURL(blob);
 
-        // Clean up by revoking the Object URL and removing the link
-        URL.revokeObjectURL(url);
-        document.body.removeChild(a);
-    } else {
-        console.log('No location data to download.');
+            // Create a link and trigger the download
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'locations.txt';
+            document.body.appendChild(a);
+            a.click();
+
+            // Clean up by revoking the Object URL and removing the link
+            URL.revokeObjectURL(url);
+            document.body.removeChild(a);
+
+            // トラッキング修了時刻を表示
+            const now = new Date();
+            const formattedTime = now.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' });
+            document.getElementById('trackingEndTime').innerText = `Tracking ended at: ${formattedTime}`;
+            
+            document.getElementById('trackingButton').disabled = true;
+            // ボタンの表示を元に戻す
+            //document.getElementById('trackingButton').innerText = 'Done';
+            //isTracking = false;
+        } else {
+            console.log('No location data to download.');
+        }
+
+        // ボタンの表示を元に戻す
+        document.getElementById('trackingButton').innerText = 'Start Tracking';
+        isTracking = false;
     }
-}
+
+    // イベントリスナーを追加
+    document.getElementById('trackingButton').addEventListener('click', function () {
+        if (isTracking) {
+            downloadData();
+        } else {
+            startTracking();
+        }
+    });
+});
