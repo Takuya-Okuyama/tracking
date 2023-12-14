caa2a28cadfdad2690f9b23e4ae59e2870d326f7
Takuya-Okuyama <95153595+Takuya-Okuyama@users.noreply.github.com>
2023-12-13 22:02:40 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,10 @@ if ('serviceWorker' in navigator) {
 // IDを格納するための変数、watchPosition関数から得られる
 let watchId = null;
 
-// 最後にデータを保存した時刻
-let lastSavedTime = Date.now();
+// 最後にデータを保存した時刻として、UNIXエポック時間の開始点を指定する
+let lastSavedTime = Date(0);
+let prev_lat = null;
+let prev_lon = null;
 
 // トラッキングの状態（開始/停止）
 let isTracking = false;
@@ -45,6 +47,22 @@ dbRequest.onupgradeneeded = function (event) {
     }
 };
 
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    var R = 6371e3; // 地球の半径 (メートル)
+    var φ1 = lat1 * Math.PI/180; // 緯度1のラジアン値
+    var φ2 = lat2 * Math.PI/180; // 緯度2のラジアン値
+    var Δφ = (lat2-lat1) * Math.PI/180; // 緯度差のラジアン値
+    var Δλ = (lon2-lon1) * Math.PI/180; // 経度差のラジアン値
+
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+            Math.cos(φ1) * Math.cos(φ2) *
+            Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    var distance = R * c; // 結果 (メートル)
+    return distance;
+}
+
 // 位置情報を取得してローカルストレージに保存する関数
 function saveLocation(position) {
     const currentTime = Date.now();
@@ -60,6 +78,13 @@ function saveLocation(position) {
         spd: position.coords.speed || null
     };
 
+    let estimated_speed = null;
+    if (prev_lat != null && prev_lon != null && locationData.lat != null && locationData.lon != null) {
+        let elapsed_time = (currentTime - lastSavedTime) / (3600 * 1e3); // hour
+        let distance = calculateDistance(prev_lat, prev_lon, locationData.lat, locationData.lon) * 1e-3; // km
+        estimated_speed = distance / elapsed_time;
+    }
+
     // ダッシュボードに最新情報を表示
     const now = new Date();
     document.getElementById('timestamp').innerText = now.toLocaleString('en-US', {
@@ -74,6 +99,7 @@ function saveLocation(position) {
     document.getElementById('altitudeAccuracy').innerText = locationData.altacc !== null ? (locationData.altacc.toFixed(1) + " m") : "N/A";
     document.getElementById('heading').innerText = locationData.head !== null ? round(locationData.head) : "N/A";
     document.getElementById('speed').innerText = locationData.spd !== null ? (locationData.spd.toFixed(1) + " m/s") : "N/A";
+    document.getElementById('estimated_speed').innerText = estimated_speed !== null ? (estimated_speed.toFixed(1) + " km/h") : "N/A";
 
     // 前回の保存から10秒以上経過しているか確認
     if (currentTime - lastSavedTime >= sampling_interval) {
@@ -93,7 +119,7 @@ function handleError(error) {
 document.addEventListener('DOMContentLoaded', function () {
     let isTracking = false;
 
-    async function startTracking() {
+    function startTracking() {
         // ファイル名
         const now = new Date();
         saveFilePath = "LocationTracker_" + now.toLocaleString('en-US', {
@@ -127,7 +153,7 @@ document.addEventListener('DOMContentLoaded', function () {
         isTracking = true;
     }
 
-    async function downloadData() {
+    function downloadData() {
         let transaction = db.transaction(["locations"], "readonly");
         let store = transaction.objectStore("locations");
         let request = store.getAll();
