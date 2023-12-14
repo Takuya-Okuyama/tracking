8aefb797b07eb0afcf1c08725dc218fd37ad1855
Takuya-Okuyama <95153595+Takuya-Okuyama@users.noreply.github.com>
2023-12-13 21:23:44 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('service_worker.js').then(function (registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
 // IDを格納するための変数、watchPosition関数から得られる
 let watchId = null;
 
